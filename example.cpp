4961221153f2b228565cb350712c31e2aeba6e20
root <root@localhost.localhost>
2015-11-01 20:18:12 -0500

    added

--- example.cpp
+++ example.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main() {
 	cout << "Hello World" << endl;
+	second();
+	return 0; 
+                 
+}
+int second() {
+	cout << "And Goodbye" << endl;
 
 	return 0;
 }
